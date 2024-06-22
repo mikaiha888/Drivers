666eafdbd272a1fea7dae5eca36c3d4a1c521b70
Mika <test@Tests-MacBook-Pro.local>
2024-06-22 00:20:08 -0300

    feat(models): add db models with validations

--- server/src/models/Drivers.js
+++ server/src/models/Drivers.js
@@ -9,6 +9,64 @@ module.exports = (sequelize) => {
         primaryKey: true,
         defaultValue: DataTypes.UUIDV4,
       },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isAlpha: {
+            msg: "First name should only contain letters",
+          },
+          len: {
+            args: [2, 32],
+            msg: "First name should be between 2 and 32 characters",
+          },
+        },
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isAlpha: {
+            msg: "Last name should only contain letters",
+          },
+          len: {
+            args: [2, 32],
+            msg: "Last name should be between 2 and 32 characters",
+          },
+        },
+      },
+      dob: {
+        type: DataTypes.DATEONLY,
+        allowNull: false,
+      },
+      nationality: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isAlpha: {
+            msg: "Nationality should only contain letters",
+          },
+          len: {
+            args: [2, 32],
+            msg: "Nationality should be between 2 and 32 characters",
+          },
+        },
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: {
+            args: [5],
+            msg: "Description should contain at least 5 letters"
+          }
+        }
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        isUrl: true,
+      },
     },
     { timestamps: false }
   );
--- server/src/models/Teams.js
+++ server/src/models/Teams.js
@@ -9,6 +9,17 @@ module.exports = (sequelize) => {
         primaryKey: true,
         defaultValue: DataTypes.UUIDV4,
       },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          len: {
+            args: [2, 32],
+            msg: "Team name should be between 2 and 32 characters",
+          },
+        },
+      },
     },
     { timestamps: false }
   );
