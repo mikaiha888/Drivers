4160437a963352d712b8a52dce4c68dc64da8ed0
Mika <test@Tests-MacBook-Pro.local>
2024-06-23 18:33:10 -0300

    refactor(tests): refactor unit test for models

--- server/tests/models/driversModel.test.js
+++ server/tests/models/driversModel.test.js
@@ -1,14 +1,18 @@
 const { Driver } = require("../../src/models/Drivers");
 const { expect } = require("jest");
 
-const driverData = {
-  firstName: "John",
-  lastName: "Doe",
-  dob: "1990-01-01",
-  nationality: "American",
-  description: "Experienced driver",
-  image: "http://example.com/image.jpg",
-};
+let driverData;
+
+beforeEach(() => {
+  driverData = {
+    firstName: "John",
+    lastName: "Doe",
+    dob: "1990-01-01",
+    nationality: "American",
+    description: "Experienced driver",
+    image: "http://example.com/image.jpg",
+  };
+});
 
 describe("Driver Model", () => {
   it("should create a driver with valid attributes", async () => {
@@ -24,80 +28,32 @@ describe("Driver Model", () => {
   });
 
   it("should not create a driver with invalid firstName", async () => {
-    await expect(
-      Driver.create({
-        firstName: "J1",
-        lastName: "Doe",
-        dob: "1990-01-01",
-        nationality: "American",
-        description: "Experienced driver",
-        image: "http://example.com/image.jpg",
-      })
-    ).rejects.toThrow();
+    driverData.firstName = "J1";
+    await expect(driverData).rejects.toThrow();
   });
 
   it("should not create a driver with invalid lastName", async () => {
-    await expect(
-      Driver.create({
-        firstName: "John",
-        lastName: "D1",
-        dob: "1990-01-01",
-        nationality: "American",
-        description: "Experienced driver",
-        image: "http://example.com/image.jpg",
-      })
-    ).rejects.toThrow();
+    driverData.lastName = "D1";
+    await expect(Driver.create(driverData)).rejects.toThrow();
   });
 
   it("should not create a driver with invalid dob", async () => {
-    await expect(
-      Driver.create({
-        firstName: "John",
-        lastName: "Doe",
-        dob: 19900101,
-        nationality: "American",
-        description: "Experienced driver",
-        image: "http://example.com/image.jpg",
-      })
-    ).rejects.toThrow();
+    driverData.dob = 19900101;
+    await expect(driverData).rejects.toThrow();
   });
 
   it("should not create a driver with invalid nationallity", async () => {
-    await expect(
-      Driver.create({
-        firstName: "John",
-        lastName: "Doe",
-        dob: "1990-01-01",
-        nationality: "A1",
-        description: "Experienced driver",
-        image: "http://example.com/image.jpg",
-      })
-    ).rejects.toThrow();
+    driverData.nationality = "A1";
+    await expect(driverData).rejects.toThrow();
   });
 
   it("should not create a driver with invalid description", async () => {
-    await expect(
-      Driver.create({
-        firstName: "John",
-        lastName: "Doe",
-        dob: "1990-01-01",
-        nationality: "American",
-        description: "Test",
-        image: "http://example.com/image.jpg",
-      })
-    ).rejects.toThrow();
+    driverData.description = "Test";
+    await expect(driverData).rejects.toThrow();
   });
 
   it("should not create a driver with invalid image URL", async () => {
-    await expect(
-      Driver.create({
-        firstName: "John",
-        lastName: "Doe",
-        dob: "1990-01-01",
-        nationality: "American",
-        description: "Experienced driver",
-        image: "invalid-url",
-      })
-    ).rejects.toThrow();
+    driverData.image = "invalid-url";
+    await expect(driverData).rejects.toThrow();
   });
 });
