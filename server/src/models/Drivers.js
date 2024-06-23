3378cb9bb757d715eb77f2ed6b260a7f7b313bf3
Mika <test@Tests-MacBook-Pro.local>
2024-06-23 18:39:28 -0300

    refactor(models): rename drivers and teams models

--- server/src/models/Drivers.js
+++ server/src/models/Drivers.js
@@ -2,7 +2,7 @@ const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
   sequelize.define(
-    "driver",
+    "Driver",
     {
       id: {
         type: DataTypes.UUID,
--- server/src/models/Teams.js
+++ server/src/models/Teams.js
@@ -2,7 +2,7 @@ const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
   sequelize.define(
-    "team",
+    "Team",
     {
       id: {
         type: DataTypes.UUID,
