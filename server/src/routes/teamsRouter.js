6ab0fd3c267acf3312cd1c2aaeedfa4a1b3bfeae
Mika <test@Tests-MacBook-Pro.local>
2024-06-25 13:28:17 -0300

    feat(routes): add drivers and teams routes

---
+++ server/src/routes/teamsRouter.js
@@ -0,0 +1,11 @@
+const { Router } = require("express");
+
+const teamsRouter = Router();
+// teamsRouter.get("/search");
+// teamsRouter.get("/");
+// teamsRouter.get("/:id");
+// teamsRouter.post("/");
+// teamsRouter.put("/");
+// teamsRouter.delete("/:id");
+
+module.exports = teamsRouter;
